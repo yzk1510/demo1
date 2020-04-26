dcaa37759614aef8d9c0da2bb4ec0b4f53c573a2
你的名字 <>
2020-04-26 23:31:52 +0800

    yzk

--- src/main/java/com/example/demo/util/Test.java
+++ src/main/java/com/example/demo/util/Test.java
@@ -1,6 +1,9 @@
 package com.example.demo.util;
 
 public class Test {
+    public void test1(){
+        System.out.println("分支测试");
+    }
     public void test(){
         System.out.println("分支测试");
     }
